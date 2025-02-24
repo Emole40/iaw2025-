8638903f5e7c426b4212d54ace87d456d60e68cd
Emole40 <eusebiomorenoleon@gmail.com>
2025-02-24 14:13:29 +0100

    Variables

--- holamundo.php
+++ holamundo.php
@@ -1,13 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Hola Mundo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Hola Mundo</title>
 </head>
 <body>
-	<?php
-		print("Hello World! From IES Poligono Sur");
-	?>
+    <?php
+    // Definir la constante con comillas
+    define("SALUDO", "Bienvenido al IES Poligono Sur");
+    
+    // Imprimir el saludo y añadir un salto de línea
+    echo SALUDO . "<br><br>";
+
+    // Declarar variables
+    $texto = "Este es un ejemplo de texto.";
+	echo "$texto";
+	var_dump($texto);
+	echo "<br</br>";
+    $nombre = "Eusebio Moreno";
+	echo "$nombre";
+	var_dump($nombre);
+    $boolean = true;
+	echo "$boolean";
+	var_dump($boolean);
+    ?>
 </body>
 </html>
